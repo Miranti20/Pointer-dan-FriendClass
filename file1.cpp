6b4757ecde7b61a195fe5bbeb3b809a5416359a5
Miranti20 <rantirarrr4505@gmail.com>
2025-05-06 10:53:26 +0700

    Class Lingkaran

--- file1.cpp
+++ file1.cpp
@@ -17,10 +17,37 @@ public:
     {
     
     int luasPp(int a,int b)
-    {
+    
         return a * b;
     }
 
     Lingkaran ol;
+
+    };
+
+    class Lingkaran
+    {
+    public:
+        int jarijari;
+
+        void input()
+        {
+            cout << "Masukkan jari-jari : ";
+            cin >> jarijari;
+        }
+
+        int luasLingkaran(int r)
+        {
+            return 3.14 * r * r;
+        }
+    }
+
+};
+
+    int main ()
+}
+
+    
+    
 
     
